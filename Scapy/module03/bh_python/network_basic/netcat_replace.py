a1e92c6d66db4c55f2dfd94f093fae802526d817
Haydn <haydnit@gmail.com>
2015-10-06 19:16:32 -0400

    trouble shooting scripts started netcat replacement

--- Scapy/module03/bh_python/network_basic/netcat_replace.py
+++ Scapy/module03/bh_python/network_basic/netcat_replace.py
@@ -3,17 +3,18 @@ __author__ = 'root'
 import sys
 import socket
 import getopt
-import threadingimport subprocess
+import threading
+import subprocess
 
-#define global variables
-listen = False
-command = False
-upload = False
-execute = ""
-target = ""
-#target = ""
-upload_destination =""
-port =0
+# #define global variables
+# listen = False
+# command = False
+# upload = False
+# execute = ""
+# target = ""
+# #target = ""
+# upload_destination =""
+# port =0
 
 def usage():
     print "From BHP net tool"
@@ -99,12 +100,90 @@ def client_handler(client_socket):
     #check for upload
     if len(upload_destination):
 
+        #read all bytes and write to destination
+        file_buffer = ""
+
+        #keep reading data until non is available?
+
+        while TRUE:
+            data = client_socket.recv(1024)
+
+            if not data:
+                break
+
+            else:
+                file_buffer += data
+
+        #take bytes and try to write them
+        try:
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+
+            #acknowledge successful
+            client_socket.send("Successfully saved file to %s\r\n:" % upload_destination)
+
+        except:
+            client_socket.sent("Failed to save file to %s\r\n" % upload_destination)
 
 
-def main()
+    if len(execute):
+        #run command
+        output = run_command(execute)
+
+        client_socket.send(output)
+
+    #another loop ifcommand shell was requested
+
+    if command:
+        while True:
+            #show a simple prompt
+            client_socket.send("<BHP:#> ")
+
+            cmd_buffer=""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+
+            #send command outpuit
+            response = run_command(cmd_buffer)
+
+            #send back the response
+            client_socket.send(response)
+
+
+
+
+
+if __name__ == "__main__":
+
+    #define global variables
+    listen = False
+    command = False
+    upload = False
+    execute = ""
+    target = ""
+    #target = ""
+    upload_destination =""
+    port =0
+
+    # global listen
+    # global port
+    # global execute
+    # global command
+    # global upload_destination
+    # global target
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu", ["help","listen","execute","target","port","command", \
+                                                                 "upload"])
+
+    except getopt.GetoptError as err:
+        print str(err)
+
+
 
     for o,a,in opts:
-        if o in ("h","--help"):
+        if o in ("-h","--help"):
             usage()
         elif o in ("-l","--listen"):
             listen = True
@@ -113,9 +192,11 @@ def main()
         elif o in ("-c", "--commandshell"):
             command = True
         elif o in ("-u", "--upload"):
-            upload _destination = a
+            upload_destination = a
         elif o in ("-t", "--target"):
             target = a
+        elif o in ("-p", "--ports"):
+            port = int(a)
         else:
             assert False, "Unhandled Option"
 
@@ -133,7 +214,7 @@ def main()
         #listen and potentially upload, execute, drop a shell
 
     if not listen:
-        server loop()
+        server_loop()
 
-main()
+
 
